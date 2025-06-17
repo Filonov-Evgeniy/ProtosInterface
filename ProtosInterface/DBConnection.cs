b1737defa9b5a6deb2938823729a2b6ba3ee36ba
Filonov-Evgeniy <advent.alpen@gmail.com>
2025-06-17 23:05:25 +0300

    connect

--- ProtosInterface/DBConnection.cs
+++ ProtosInterface/DBConnection.cs
@@ -4,16 +4,18 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace ProtosInterface
 {
     class DBConnection
     {
         //private readonly string connectionString = @"Data Source=KAYSER\MSSQLSERV;Initial Catalog=TestDB;Integrated Security=True;TrustServerCertificate=true;";
-        private readonly string connectionString = @"Server=192.168.0.106;Database=TestMainDB;Integrated Security=True;TrustServerCertificate=true;";
+        //private readonly string connectionString = @"Server=192.168.0.106;Database=TestMainDB;Integrated Security=True;TrustServerCertificate=true;";
         //private readonly string connectionString = @"Data Source=KAYSER\MSSQLSERV;Initial Catalog=TestMainDB;Integrated Security=True;TrustServerCertificate=true;";
         //private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestDB;Integrated Security=True;TrustServerCertificate=true;";
-        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProtosDB;Integrated Security=True;Trust Server Certificate=true;";
+        //private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProtosDB;Integrated Security=True;Trust Server Certificate=true;";
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = ProtosDBActual; Integrated Security = True; Trust Server Certificate=true;";
         public string ConnectionString
         {
             get { return connectionString; }
